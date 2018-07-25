b193cb9d677e94da81a6c67c2837c0c763b3f9e7
chirag1603 <chiragchoudha1603@gmail.com>
2018-07-25 14:34:49 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('chirag1603')
+            node / gitConfigEmail('chiragchoudha1603@gmail.com')
         }
     }
     triggers {
